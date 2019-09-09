2157bd637bc6e1ea0622db734bbbb224730799d6
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-09 15:08:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,9 +100,9 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }w#change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     }
+     programmer_hash[alan_kay][:known_for]= "GUI"
+programmer_hash     
      
 end
 
